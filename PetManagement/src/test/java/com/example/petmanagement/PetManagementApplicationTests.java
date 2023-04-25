b75d4b5e0ce0be672fbeecd469ed3aa3905db942
lingtregetter <lingtregetter466@gmail.com>
2023-04-25 14:41:10 +0300

    initial and .gitignore

---
+++ PetManagement/src/test/java/com/example/petmanagement/PetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
