01d4c1c32d43f87a8c779b4b8a9fc8b42cc4ebc9
lingtregetter <lingtregetter466@gmail.com>
2023-05-06 12:25:44 +0300

    Pet entity update

--- PetManagement/src/main/java/com/example/petmanagement/domain/Pet.java
+++ PetManagement/src/main/java/com/example/petmanagement/domain/Pet.java
@@ -2,44 +2,48 @@ package com.example.petmanagement.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "pet")
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "Name is required!")
+    @NotEmpty(message = "Name field cannot be empty!")
     @Column(name = "name", nullable = false)
     private String name;
 
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "Code is required!")
+    @NotEmpty(message = "Code field cannot be empty!")
+    @Size(min = 12, max = 12)
     @Column(name = "code", nullable = false)
     private String code;
 
-    @NotNull
+    @NotNull(message = "Type is required!")
     @ManyToOne
     @JoinColumn(name = "type_id", nullable = false)
     private PetType type;
 
-    @NotNull
+    @NotNull(message = "Color is required!")
     @ManyToOne
     @JoinColumn(name = "color_id", nullable = false)
     private PetColor furColor;
 
-    @NotNull
+    @NotNull(message = "Country of origin is required!")
     @ManyToOne
     @JoinColumn(name = "country_id", nullable = false)
     private PetCountry countryOfOrigin;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name = "account_id", nullable = true)
+    @JoinColumn(name = "account_id", nullable = false)
     private UserAccount userAccount;
 }
