f4e0491e97ae9e6573d175315d73145efb91d2b0
lingtregetter <lingtregetter466@gmail.com>
2023-05-06 12:38:54 +0300

    error controller updated

--- PetManagement/src/main/java/com/example/petmanagement/ErrorHandlers/ErrorController.java
+++ PetManagement/src/main/java/com/example/petmanagement/ErrorHandlers/ErrorController.java
@@ -5,17 +5,34 @@ import com.example.petmanagement.exceptions.CustomNullPointerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class PetErrorController {
+public class ErrorController {
 
     @ExceptionHandler(CustomNullPointerException.class)
     private ResponseEntity<ErrorResponse> handleCustomNullPointerException(Exception e) {
         return responseEntityJson(HttpStatus.NOT_FOUND, e);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldKey = ((FieldError) error).getField();
+            String fieldValue = error.getDefaultMessage();
+            errors.put(fieldKey, fieldValue);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<ErrorResponse> responseEntityJson(HttpStatus status, Exception e) {
