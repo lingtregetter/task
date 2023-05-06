c983227e25a3db9f5e47f3cc4cf3163e3fad5986
lingtregetter <lingtregetter466@gmail.com>
2023-05-06 12:29:10 +0300

    User entity update

--- PetManagement/src/main/java/com/example/petmanagement/domain/UserAccount.java
+++ PetManagement/src/main/java/com/example/petmanagement/domain/UserAccount.java
@@ -1,19 +1,23 @@
 package com.example.petmanagement.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Entity
-@Table(name = "account")
+@Table(name = "users")
 public class UserAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Username is required!")
+    @NotEmpty(message = "Username field cannot be empty!")
     private String username;
 
+    @NotNull(message = "Password is required!")
+    @NotEmpty(message = "Password field cannot be empty!")
     private String password;
 }
